0938d4627d14d2c2a9c6f4bf47747e1cfcf66e32
AIDcGH <aviv.c000@gmail.com>
2025-05-12 00:16:40 +0300

    added basic structure to gigdetails

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -9,7 +9,7 @@ import { loadGig, addGigMsg } from '../store/actions/gig.actions'
 
 export function GigDetails() {
 
-  const {gigId} = useParams()
+  const { gigId } = useParams()
   const gig = useSelector(storeState => storeState.gigModule.gig)
 
   useEffect(() => {
@@ -18,26 +18,39 @@ export function GigDetails() {
 
   async function onAddGigMsg(gigId) {
     try {
-        await addGigMsg(gigId, 'bla bla ' + parseInt(Math.random()*10))
-        showSuccessMsg(`Gig msg added`)
+      await addGigMsg(gigId, 'bla bla ' + parseInt(Math.random() * 10))
+      showSuccessMsg(`Gig msg added`)
     } catch (err) {
-        showErrorMsg('Cannot add gig msg')
-    }        
+      showErrorMsg('Cannot add gig msg')
+    }
 
-}
+  }
 
   return (
     <section className="gig-details">
-      <Link to="/gig">Back to list</Link>
-      <h1>Gig Details</h1>
-      {gig && <div>
-        <h3>{gig.title}</h3>
-        <h4>{gig.price}</h4>
-        <pre> {JSON.stringify(gig, null, 2)} </pre>
-      </div>
-      }
-      <button onClick={() => { onAddGigMsg(gig._id) }}>Add gig msg</button>
-
+      <section className="gig-info">
+        <nav className="breadcrumbs">
+          <Link to="/">Home</Link>
+          <Link to="/gig">Back to list</Link>
+        </nav>
+        <h1>Gig Title</h1>
+        <article className="mini-user"></article>
+        <article className="img-carousel"></article>
+        <article className="about-section"></article>
+        <article className="about-user"></article>
+        <section className="reviews">
+          <article className="review-statistics"></article>
+          <ReviewList />
+        </section>
+      </section>
+      <section className="purchase-modal">
+        <article>
+          Add to Favorites ♥
+        </article>
+        <article className="purchase-modal">
+          buy 
+        </article>
+      </section>
     </section>
   )
 }
