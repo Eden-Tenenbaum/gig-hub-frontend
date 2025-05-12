65f4f0450740ea2f425c109c632f7f1f6cfe28e5
Yonatan Keren <yonatan.keren1@gmail.com>
2025-05-12 07:39:09 +0300

    Add fontawesome for basic icons

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,12 +1,17 @@
 import { Link, NavLink } from 'react-router-dom'
-import { useNavigate } from 'react-router'
+import { useLocation, useNavigate } from 'react-router'
 import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { logout } from '../store/actions/user.actions'
+import { useEffect, useState } from 'react'
 
-export function AppHeader() {
+export function AppHeader() { //get logged in status from home page
+	const location = useLocation()
 	const user = useSelector(storeState => storeState.userModule.user)
 	const navigate = useNavigate()
+	const [showSearchBar, SetShowSearchBar] = useState(false)
+	const [loggedIn, setLoggedIn] = useState(true) // band aid function, 
+	// should get logged in state from another cmp
 
 	async function onLogout() {
 		try {
@@ -18,30 +23,41 @@ export function AppHeader() {
 		}
 	}
 
+	function toggleLoggedIn(status){ //another band-aid function until log in status is set up
+		if(status === true){ 
+			setLoggedIn(false)
+			SetShowSearchBar(false)
+			return
+		}
+		if(status === false){
+			setLoggedIn(true)
+			SetShowSearchBar(true)
+			return
+		}
+	}
+
 	return (
 		<header className="app-header full">
 			<nav>
 				<NavLink to="/" className="logo">
-					E2E Demo
+					Diverr.
 				</NavLink>
+				{showSearchBar &&
+					<div>
+						<input type='text' placeholder='What service are you looking for today?' />
+					</div>}
 				<NavLink to="about">About</NavLink>
 				<NavLink to="gig">Gigs</NavLink>
 				<NavLink to="chat">Chat</NavLink>
 				<NavLink to="review">Review</NavLink>
 
-                {user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
+				{user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
 
-				{!user && <NavLink to="login" className="login-link">Login</NavLink>}
-				{user && (
-					<div className="user-info">
-						<Link to={`user/${user._id}`}>
-							{/* {user.imgUrl && <img src={user.imgUrl} />} */}
-							{user.fullname}
-						</Link>
-						{/* <span className="score">{user.score?.toLocaleString()}</span> */}
-						<button onClick={onLogout}>logout</button>
-					</div>
-				)}
+				<div className='login-bandaid'>
+					<button onClick={() => toggleLoggedIn(loggedIn)}>Join</button>
+					<span className={`login-circle ${loggedIn ? 'active' : ''}`}><i className="fa-solid fa-circle"></i></span>
+				</div>
+
 			</nav>
 		</header>
 	)
