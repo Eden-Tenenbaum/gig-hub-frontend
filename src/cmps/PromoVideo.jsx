d011d9aae4867c41a5ce0e53d06007128bee0d43
Yonatan Keren <yonatan.keren1@gmail.com>
2025-05-13 17:34:33 +0300

    Adjust home page

--- src/cmps/PromoVideo.jsx
+++ src/cmps/PromoVideo.jsx
@@ -6,35 +6,6 @@ export function PromoVideo() {
             <video autoPlay muted loop>
                 <source src={promoVideo} type="video/mp4" />
             </video>
-            <section className='over-promo-video'>
-                <div className='main-over-promo-video-content'>
-                    <p>Our freelancers will take it from here</p>
-                    <input
-                        type='text'
-                        placeholder='Search for any service'
-
-                    />
-                    <button className='promo-video-search-bar-btn'>
-                        <i className="fa-solid fa-magnifying-glass"></i>
-                    </button>
-
-                    <div className='over-video-filter-btns'>
-                        <button>website development <span><i className="fa-solid fa-arrow-right"></i></span></button>
-                        <button>architecture & interior design <span><i className="fa-solid fa-arrow-right"></i></span></button>
-                        <button>UGC videos <span><i className="fa-solid fa-arrow-right"></i></span></button>
-                        <button>video editing <span><i className="fa-solid fa-arrow-right"></i></span></button>
-                    </div>
-                </div>
-                <div className='trusted-by'>
-                    Trusted by: 
-                    <span>Meta icon</span>
-                    <span>Google icon</span>
-                    <span>Netflix icon</span>
-                    <span>Proctor & Gamble icon</span>
-                    <span>Paypal icon</span>
-                    <span>Payoneer icon</span>
-                </div>
-            </section>
         </section>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import WebsiteDevelopment from '../../public/img/website-development.webp'
+
 const fiverrMainCategories = [
     { name: "Programming & Tech", id: "programming-tech" },
     { name: "Graphics & Design", id: "graphics-design" },
@@ -17,15 +19,55 @@ const fiverrMainCategories = [
 export function HomePage() {
     return (
         <section>
+            <section className='over-promo-video'>
+                <div className='main-over-promo-video-content'>
+                    <div>
+                        <p>Our freelancers</p>
+                        <p>will take it from here</p>
+                    </div>
+                    <div className="searchbar-over-promo-video">
+                        <input
+                            type='text'
+                            placeholder='Search for any service'
+
+                        />
+                        <button className='promo-video-search-bar-btn'>
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+
+                    <div className='over-video-filter-btns'>
+                        <div><a>website development </a><span><i className="fa-solid fa-arrow-right"></i></span></div>
+                        <div><a>architecture & interior design </a><span><i className="fa-solid fa-arrow-right"></i></span></div>
+                        <div><a>UGC videos </a><span><i className="fa-solid fa-arrow-right"></i></span></div>
+                        <div><a>video editing </a><span><i className="fa-solid fa-arrow-right"></i></span></div>
+                    </div>
+                </div>
+                <div className='trusted-by'>
+                    Trusted by:
+                    <span>Meta icon</span>
+                    <span>Google icon</span>
+                    <span>Netflix icon</span>
+                    <span>Proctor & Gamble icon</span>
+                    <span>Paypal icon</span>
+                    <span>Payoneer icon</span>
+                </div>
+            </section>
             <div className="homepage-categories">
-                    {fiverrMainCategories.map(category =>
-                        <div className={`${category.id}`}>
-                            <a>{category.name}</a>
-                        </div>
-                    )}
+                {fiverrMainCategories.map(category =>
+                    <div key={category.id}>
+                        <a>{category.name}</a>
+                    </div>
+                )}
             </div>
             <div className="homepage-popular-services">
                 <p>Popular services</p>
+                <div className="popular-service-card">
+                    <a>
+                        <p>Website Development</p>
+                        <img src={WebsiteDevelopment}/>
+                    </a>
+                </div>
             </div>
         </section >
     )
