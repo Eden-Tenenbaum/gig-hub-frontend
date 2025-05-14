1ba9b6c7306e8bfa202d419d1188e0cf1bebf70b
Yonatan Keren <yonatan.keren1@gmail.com>
2025-05-14 12:50:52 +0300

    Add bottom join section to home page

---
+++ src/cmps/BottomHomePageAd.jsx
@@ -0,0 +1,8 @@
+export function BottomHomePageAd(){
+    return (
+        <section className="bottom-homepage-join-ad">
+            <div><span>Freelance services at your </span><a> fingertips</a></div>
+            <button>Join Diverr</button>
+        </section>
+    )
+}
