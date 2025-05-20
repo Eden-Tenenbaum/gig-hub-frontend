1f817e1c01ef87f83f3ef912703a1553dab96ab1
AIDcGH <aviv.c000@gmail.com>
2025-05-20 20:41:10 +0300

    Added empty and half stars for rating

--- src/cmps/Stars.jsx
+++ src/cmps/Stars.jsx
@@ -1,31 +1,38 @@
-import { StarIcon } from './SvgHub'
+import { EmptyStarIcon, HalfStarIcon, StarIcon } from './SvgHub'
 
 export function Stars({ rating, showStars = false, reviewCount = { isReview: false, underline: false, link: false }, starProps = {} }) {
 
   const movesToReviews = () => {
     if (!reviewCount.link) return
 
-    if (location.hash === '#reviews') history.replaceState(null, '', location.pathname + location.search);
+    if (location.hash === '#reviews') history.replaceState(null, '', location.pathname + location.search)
 
-    location.hash = '#reviews';
+    location.hash = '#reviews'
   }
 
+  const roundToHalf = Math.round(rating * 2) / 2
+  const fullStars = Math.floor(roundToHalf)
+  const hasHalf = roundToHalf % 1 !== 0
+  const emptyStars = 5 - fullStars - (hasHalf ? 1 : 0)
+  
   return (
     <section className="stars-display">
       {showStars && (
         <span className="stars">
-          {[...Array(Math.round(rating))].map((_, idx) => <StarIcon key={idx} {...starProps} />)}
+          {[...Array(fullStars)].map((_, i) => <StarIcon key={`full-${i}`} {...starProps} />)}
+          {hasHalf && <HalfStarIcon key="half" {...starProps} />}
+          {[...Array(emptyStars)].map((_, i) => <EmptyStarIcon key={`empty-${i}`} {...starProps} />)}
         </span>
       )}
       <span className="rating-num stars">
         {!showStars && <StarIcon {...starProps} />}
         {rating}
       </span>
-      {!reviewCount.isReview &&
+      {!reviewCount.isReview && (
         <span className="review-count" onClick={movesToReviews}>
-          {'('}<span className={reviewCount.underline && 'underline'}>19</span>{')'}
+          {'('}<span className={reviewCount.underline ? 'underline' : ''}>19</span>{')'}
         </span>
-      }
+      )}
     </section>
   )
 }
