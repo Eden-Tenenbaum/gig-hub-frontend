218274f4bea04aa97a464603df33f253d6037323
AIDcGH <aviv.c000@gmail.com>
2025-05-20 16:23:37 +0300

    Fixed img carousel in details and added a helper css class

--- src/cmps/gigDetails/ImgCarousel.jsx
+++ src/cmps/gigDetails/ImgCarousel.jsx
@@ -1,11 +1,13 @@
 export function ImgCarousel({ gig }) {
     return <article className="img-carousel grid place-center">
         <section className="img-container-carousel grid place-center">
-            <img src={gig.imgUrl} alt="img" />
+            <img src={gig.imgUrl[0].src} alt="img" />
         </section>
         <section className="thumbnails">
-            <ul className="thumbnails-list">
-                <li className="grid place-center"><img src={gig.imgUrl} alt="img" /></li>
+            <ul className="thumbnails-list flex">
+                {gig.imgUrl.map(img => 
+                    <li key={img} className="grid place-center"><img src={img.src} alt="img" /></li>
+                )}
             </ul>
         </section>
     </article>
